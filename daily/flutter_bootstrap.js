c0b9e3822c80389b3d054bbcb19c39d05af89dd1
vprapra <valerie.pracchia@gmail.com>
2026-06-23 20:25:58 +0200

    web-play: visuel monnaie + énigmes expliquées + 'Voir mon score' sur la dernière

--- daily/flutter_bootstrap.js
+++ daily/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4211831519" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4194463308" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
