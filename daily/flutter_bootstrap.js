02e7518627e45247d89e3bc7ed94d690e2d99b36
vprapra <valerie.pracchia@gmail.com>
2026-06-23 21:45:49 +0200

    web-play: soustraction posée avec emprunt animé

--- daily/flutter_bootstrap.js
+++ daily/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1365732940" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3957570611" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
