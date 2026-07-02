45a06ccbd109b4d79c22d63b5f92cb636a05ef8d
Valerie Pracchia <valerie.pracchia@werebase.fr>
2026-07-02 10:52:56 +0200

    Publish web-play: per-question analytics, participants view removed

--- daily/flutter_bootstrap.js
+++ daily/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "210415023" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "520449116" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
